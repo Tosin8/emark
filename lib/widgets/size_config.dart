49ffdcc71c72a1ce7aade4bb5f9db94a6a594e24
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-22 02:21:59 +0100

    Update size_config.dart

--- lib/widgets/size_config.dart
+++ lib/widgets/size_config.dart
@@ -4,4 +4,29 @@ class SizeConfig {
   static MediaQueryData _mediaQueryData;
   static double screenWidth;
   static double screenHeight;
+  static double defaultSize;
+  static Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+  }
+}
+
+// getting the proportionate height as per screen size
+double getProportionateScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+
+  // disclaimer: 812 is the layout height that designer use.
+  return (inputHeight / 812.0) * screenHeight;
+}
+
+// get the proportionate height as per screen size
+double getProportionateScreenWidth(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth;
+
+  // disclaimer: 375 is the layout width that designer use.
+  return (inputWidth / 375.0) * screenWidth;
 }
