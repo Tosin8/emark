fb2ae32e3995c6f26e24fd88b2ec9e8a22f53f43
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-22 01:05:37 +0100

    Adding to pubspec

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -5,6 +5,80 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/splash/1.jpg',
+                  ),
+                  colorFilter: ColorFilter.mode(
+                      Colors.white.withOpacity(0.8), BlendMode.modulate),
+                  fit: BoxFit.cover,
+                )),
+                child: Container(
+                  decoration: BoxDecoration(
+                      gradient:
+                          LinearGradient(begin: Alignment.bottomRight, stops: [
+                    0.2,
+                    0.9
+                  ], colors: [
+                    Colors.black.withOpacity(.6),
+                    Colors.black.withOpacity(.1),
+                  ])),
+                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 300),
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'EMARK',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Welcome to Emark, Let\'s shop!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(children: [
+                Container(
+                  width: 280,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: Text(
+                      'Continue',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
