b64a4515243e5acf4af2c6b2abe4edebbce471f8
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-22 02:58:06 +0100

    Creating body widget for splash screen

---
+++ lib/screen/splash/components/splash_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashBody extends StatefulWidget {
+  const SplashBody({super.key});
+
+  @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
