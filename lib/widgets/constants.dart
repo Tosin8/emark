f85c8c13c7f56ddbef1258ba15cd48ee7e58f5b3
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-22 02:02:58 +0100

    Updating constants

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+const kPrimaryColor = Color(0xff8588ff);
+const kSecondaryColor = Color(0xfffffc85);
+const kAccentColor = Color(0xffcac505);
+
+const kPrimaryGradientColor = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomRight,
+    colors: [Color(0xffffa53e), Color(0xffff7643)]);
+
+const kTextColor = Color(0xff757575);
+
+const kAnimationDuration = Duration(milliseconds: 200);
