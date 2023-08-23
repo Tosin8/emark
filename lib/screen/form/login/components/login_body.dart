a46179b7471af2c88bfc308b275fa8f1b85de290
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-23 17:45:55 +0100

    Update login_body.dart

--- lib/screen/form/login/components/login_body.dart
+++ lib/screen/form/login/components/login_body.dart
@@ -27,7 +27,7 @@ class _LoginBodyState extends State<LoginBody> {
               'Sign in with your email and password or \n continue with your social media accounts',
               textAlign: TextAlign.center,
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 15),
             SignInForm(),
           ],
         ),
@@ -44,45 +44,97 @@ class SignInForm extends StatefulWidget {
 }
 
 class _SignInFormState extends State<SignInForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
+
   @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formKey,
         child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              hintText: 'Enter your email',
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              contentPadding:
-                  const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(28),
-                borderSide: const BorderSide(color: kTextColor),
-                gapPadding: 10,
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(28),
-                borderSide: const BorderSide(color: kTextColor),
-                gapPadding: 10,
-              ),
-              suffixIcon: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 20, 20, 20
-                    // getProportionateScreenWidth(20),
-                    // getProportionateScreenWidth(20),
-                    // getProportionateScreenWidth(20),
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: kTextColor),
+                    gapPadding: 10,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: kTextColor),
+                    gapPadding: 10,
+                  ),
+                  suffixIcon: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 20, 20
+                        // getProportionateScreenWidth(20),
+                        // getProportionateScreenWidth(20),
+                        // getProportionateScreenWidth(20),
+                        ),
+                    child: SvgPicture.asset(
+                      'assets/icons/mail.svg',
+                      // height: getProportionateScreenWidth(18),
                     ),
-                child: SvgPicture.asset(
-                  'assets/icons/mail.svg',
-                  height: getProportionateScreenWidth(18),
+                  ),
                 ),
               ),
-            ),
-          )
-        ],
-      ),
-    ));
+              const SizedBox(height: 20),
+              TextFormField(
+                obscureText: true, // turning it into a password key.
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: kTextColor),
+                    gapPadding: 10,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: kTextColor),
+                    gapPadding: 10,
+                  ),
+                  suffixIcon: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 20, 20
+                        // getProportionateScreenWidth(20),
+                        // getProportionateScreenWidth(20),
+                        // getProportionateScreenWidth(20),
+                        ),
+                    child: SvgPicture.asset(
+                      'assets/icons/lock.svg',
+                      // height: getProportionateScreenWidth(18),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 350,
+                height: 55,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const Center(
+                  child: Text(
+                    'CONTINUE',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
