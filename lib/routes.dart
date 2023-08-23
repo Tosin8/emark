a0bad84f7a9856587b8317b0a0d9ab064d08b625
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-23 20:39:39 +0100

    routing the forgotpass

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,4 @@
+import 'package:emark/screen/form/forgot_pass/forgot_pass.dart';
 import 'package:emark/screen/form/login/login.dart';
 import 'package:emark/screen/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -5,4 +6,5 @@ import 'package:flutter/material.dart';
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => const SplashScreen(),
   SignIn.routeName: (context) => const SignIn(),
+  ForgotPassword.routeName:(context) => ForgotPassword(),
 };
--- lib/screen/form/forgot_pass/forgot_pass.dart
+++ lib/screen/form/forgot_pass/forgot_pass.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+  static String routeName = '/forgot-password';
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
