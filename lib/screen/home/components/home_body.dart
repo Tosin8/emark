e26205a7a961dc73ad7dc137f7084834794818e5
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-28 22:50:07 +0100

    add image

--- lib/screen/home/components/home_body.dart
+++ lib/screen/home/components/home_body.dart
@@ -29,7 +29,23 @@ class _BodyState extends State<Body> {
           text: 'Special for you',
           press: () {},
         ),
-        SpecialOfferCard()
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SpecialOfferCard(
+                  image: 'assets/images/products/image_banner_2.png',
+                  category: 'Smartphones',
+                  numOfBrands: 18,
+                  press: () {}),
+              SpecialOfferCard(
+                  image: 'assets/images/products/image_banner_3.png',
+                  category: 'Smartphones',
+                  numOfBrands: 18,
+                  press: () {}),
+            ],
+          ),
+        ),
       ],
     )));
   }
@@ -38,43 +54,53 @@ class _BodyState extends State<Body> {
 class SpecialOfferCard extends StatelessWidget {
   const SpecialOfferCard({
     super.key,
+    required this.category,
+    required this.image,
+    required this.numOfBrands,
+    required this.press,
   });
+
+  final String category, image;
+  final int numOfBrands;
+  final GestureTapCallback press;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: 242,
-        height: 100,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Stack(children: [
-            Image.asset('assets/images/products/image_banner_2.png',
-                fit: BoxFit.cover),
-            Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                    const Color(0xff343434).withOpacity(0.4),
-                    const Color(0xff343434).withOpacity(0.15),
-                  ])),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              child: Text.rich(
-                TextSpan(style: TextStyle(color: Colors.white), children: [
-                  TextSpan(
-                      text: 'Smartphone\n',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      )),
-                  TextSpan(text: '18 Brands'),
-                ]),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: SizedBox(
+          width: 242,
+          height: 100,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Stack(children: [
+              Image.asset(image, fit: BoxFit.cover),
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      const Color(0xff343434).withOpacity(0.4),
+                      const Color(0xff343434).withOpacity(0.15),
+                    ])),
               ),
-            ),
-          ]),
-        ));
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                child: Text.rich(
+                  TextSpan(style: TextStyle(color: Colors.white), children: [
+                    TextSpan(
+                        text: '$category\n',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    TextSpan(text: '$numOfBrands Brands'),
+                  ]),
+                ),
+              ),
+            ]),
+          )),
+    );
   }
 }
