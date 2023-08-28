c0b3b330d766c52a9dd374e13344999215137b5c
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-28 22:58:51 +0100

    Create product file

--- lib/screen/home/components/home_body.dart
+++ lib/screen/home/components/home_body.dart
@@ -25,82 +25,10 @@ class _BodyState extends State<Body> {
         const SizedBox(height: 20),
         const Categories(),
         const SizedBox(height: 30),
-        SectionTitle(
-          text: 'Special for you',
-          press: () {},
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              SpecialOfferCard(
-                  image: 'assets/images/products/image_banner_2.png',
-                  category: 'Smartphones',
-                  numOfBrands: 18,
-                  press: () {}),
-              SpecialOfferCard(
-                  image: 'assets/images/products/image_banner_3.png',
-                  category: 'Smartphones',
-                  numOfBrands: 18,
-                  press: () {}),
-            ],
-          ),
-        ),
+        const SpeicialOffers(),
+        const SizedBox(height: 20),
+        SectionTitle(text: 'Popular Product', press: () {})
       ],
     )));
-  }
-}
-
-class SpecialOfferCard extends StatelessWidget {
-  const SpecialOfferCard({
-    super.key,
-    required this.category,
-    required this.image,
-    required this.numOfBrands,
-    required this.press,
-  });
-
-  final String category, image;
-  final int numOfBrands;
-  final GestureTapCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20),
-      child: SizedBox(
-          width: 242,
-          height: 100,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Stack(children: [
-              Image.asset(image, fit: BoxFit.cover),
-              Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                      const Color(0xff343434).withOpacity(0.4),
-                      const Color(0xff343434).withOpacity(0.15),
-                    ])),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Text.rich(
-                  TextSpan(style: TextStyle(color: Colors.white), children: [
-                    TextSpan(
-                        text: '$category\n',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    TextSpan(text: '$numOfBrands Brands'),
-                  ]),
-                ),
-              ),
-            ]),
-          )),
-    );
   }
 }
--- lib/screen/home/components/home_widgets.dart
+++ lib/screen/home/components/home_widgets.dart
@@ -252,3 +252,96 @@ class SectionTitle extends StatelessWidget {
     );
   }
 }
+
+class SpeicialOffers extends StatelessWidget {
+  const SpeicialOffers({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SectionTitle(
+          text: 'Special for you',
+          press: () {},
+        ),
+        const SizedBox(height: 15),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SpecialOfferCard(
+                  image: 'assets/images/products/image_banner_2.png',
+                  category: 'Smartphones',
+                  numOfBrands: 18,
+                  press: () {}),
+              SpecialOfferCard(
+                  image: 'assets/images/products/image_banner_3.png',
+                  category: 'Fashion',
+                  numOfBrands: 24,
+                  press: () {}),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SpecialOfferCard extends StatelessWidget {
+  const SpecialOfferCard({
+    super.key,
+    required this.category,
+    required this.image,
+    required this.numOfBrands,
+    required this.press,
+  });
+
+  final String category, image;
+  final int numOfBrands;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: SizedBox(
+          width: 242,
+          height: 100,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Stack(children: [
+              Image.asset(image, fit: BoxFit.cover),
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                      const Color(0xff343434).withOpacity(0.4),
+                      const Color(0xff343434).withOpacity(0.15),
+                    ])),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                child: Text.rich(
+                  TextSpan(
+                      style: const TextStyle(color: Colors.white),
+                      children: [
+                        TextSpan(
+                            text: '$category\n',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        TextSpan(text: '$numOfBrands Brands'),
+                      ]),
+                ),
+              ),
+            ]),
+          )),
+    );
+  }
+}
