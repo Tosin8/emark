06fc58dcd2b902a244120e313cdd71c6a0269049
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-29 21:12:37 +0100

    Updating with RoutesName

--- lib/screen/product_details/details_screen.dart
+++ lib/screen/product_details/details_screen.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
 
+class DetailsScreen extends StatelessWidget {
+  static String routeName = '/details';
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
