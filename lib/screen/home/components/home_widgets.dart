9e6d3d27352de686a337819d5c6e7121d7196e44
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-08-29 21:33:01 +0100

    Update home_widgets.dart

--- lib/screen/home/components/home_widgets.dart
+++ lib/screen/home/components/home_widgets.dart
@@ -354,68 +354,72 @@ class ProductCard extends StatelessWidget {
     this.width = 140,
     this.aspectRetion = 1.02,
     required this.product,
+    required this.press,
   });
 
   final double width, aspectRetion;
   final Product product;
+  final GestureTapCallback press;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 20),
-      child: SizedBox(
-        width: width,
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: aspectRetion,
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: kSecondaryColor.withOpacity(0.3),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Image.asset(product.images[0]),
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              product.title,
-              style: const TextStyle(color: Colors.black),
-              maxLines: 2,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '\$${product.price}',
-                  style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: kPrimaryColor),
-                ),
-                InkWell(
-                  onTap: () {},
-                  borderRadius: BorderRadius.circular(30),
-                  child: Container(
-                    padding: const EdgeInsets.all(6),
-                    width: 28,
-                    height: 28,
-                    decoration: BoxDecoration(
-                        color: product.isFavorite
-                            ? kPrimaryColor.withOpacity(0.5)
-                            : kSecondaryColor.withOpacity(0.51),
-                        shape: BoxShape.circle),
-                    child: SvgPicture.asset(
-                      'assets/icons/heart_Icon_2.svg',
-                      color: product.isFavorite
-                          ? const Color(0xffff4848)
-                          : const Color(0xffd8dee4),
-                    ),
+      child: GestureDetector(
+        child: SizedBox(
+          width: width,
+          child: Column(
+            children: [
+              AspectRatio(
+                aspectRatio: aspectRetion,
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: kSecondaryColor.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                )
-              ],
-            )
-          ],
+                  child: Image.asset(product.images[0]),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                product.title,
+                style: const TextStyle(color: Colors.black),
+                maxLines: 2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '\$${product.price}',
+                    style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: kPrimaryColor),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    borderRadius: BorderRadius.circular(30),
+                    child: Container(
+                      padding: const EdgeInsets.all(6),
+                      width: 28,
+                      height: 28,
+                      decoration: BoxDecoration(
+                          color: product.isFavorite
+                              ? kPrimaryColor.withOpacity(0.5)
+                              : kSecondaryColor.withOpacity(0.51),
+                          shape: BoxShape.circle),
+                      child: SvgPicture.asset(
+                        'assets/icons/heart_Icon_2.svg',
+                        color: product.isFavorite
+                            ? const Color(0xffff4848)
+                            : const Color(0xffd8dee4),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
