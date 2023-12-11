854e4704f441f87fbc6af22931534f887fae1a20
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-12-11 19:57:29 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:emark/routes.dart';
 import 'package:emark/widgets/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'screen/splash/splash_screen.dart';
 
@@ -14,22 +15,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'EMART App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: "Muli",
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: kTextColor),
-        ),
-        useMaterial3: true,
-      ),
-      // home: SplashScreen(),
-      initialRoute: SplashScreen.routeName,
-      // using routes so that there will be no need of remebering names.
-      routes: routes,
-    );
+    return ScreenUtilInit(
+        designSize: Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return MaterialApp(
+            title: 'EMART App',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              scaffoldBackgroundColor: Colors.white,
+              fontFamily: "Muli",
+              textTheme: const TextTheme(
+                bodyLarge: TextStyle(color: kTextColor),
+              ),
+              useMaterial3: true,
+            ),
+            // home: SplashScreen(),
+            initialRoute: SplashScreen.routeName,
+            // using routes so that there will be no need of remebering names.
+            routes: routes,
+          );
+        });
   }
 }
