12244470ea0a459b45a0678aef17d88235e97919
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-12-11 21:25:03 +0100

    Update splash_body.dart

--- lib/screen/splash/components/splash_body.dart
+++ lib/screen/splash/components/splash_body.dart
@@ -12,7 +12,7 @@ class SplashBody extends StatefulWidget {
   State<SplashBody> createState() => _SplashBodyState();
 }
 
-class _SplashBodyState extends State<SplashBody> {
+class _SplashBodyState extends State<SplashBody> with TickerProviderStateMixin {
   List<dynamic> _shopping = [
     {
       'title': 'Find your best outfit \n and look good',
@@ -35,6 +35,21 @@ class _SplashBodyState extends State<SplashBody> {
       'image': 'assets/images/splash/2.jpg',
     },
   ];
+
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: Duration(seconds: 20),
+  )..repeat();
+
+  late final Animation<double> _animation = Tween<double>(begin: 1.0, end: 1.5)
+      .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -43,65 +58,70 @@ class _SplashBodyState extends State<SplashBody> {
           _controller.value = 0.0;
           _controller.forward();
         },
-        child: Stack(children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage(
-                'assets/images/splash/4.jpg',
+        itemBuilder: (context, index) {
+          return Container(
+            child: Stack(children: [
+              Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/splash/4.jpg',
+                  ),
+                  // colorFilter: ColorFilter.mode(
+                  //     Colors.white.withOpacity(0.8), BlendMode.modulate),
+                  fit: BoxFit.cover,
+                )),
               ),
-              // colorFilter: ColorFilter.mode(
-              //     Colors.white.withOpacity(0.8), BlendMode.modulate),
-              fit: BoxFit.cover,
-            )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 400),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                  height: 300,
-                  width: 500,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      const Text(
-                        'Find your best outfit\n and look good',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 23,
-                            fontWeight: FontWeight.w700),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 20),
-                      Text(
-                        'Shop now and get value \nfor your quality products',
-                        style: TextStyle(
-                            color: Colors.black.withOpacity(0.5), fontSize: 16),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 50),
-                      Container(
-                        height: 50,
-                        width: 300,
-                        decoration: BoxDecoration(
-                            color: Colors.blueAccent,
-                            borderRadius: BorderRadius.circular(30)),
-                        child: const Align(
-                            child: Text(
-                          'Get Started',
-                          style: TextStyle(color: Colors.white),
-                        )),
-                      ),
-                      const SizedBox(height: 20),
-                    ],
-                  )),
-            ),
-          ),
-        ]),
+              Padding(
+                padding: const EdgeInsets.only(top: 400),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      height: 300,
+                      width: 500,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const Text(
+                            'Find your best outfit\n and look good',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 23,
+                                fontWeight: FontWeight.w700),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 20),
+                          Text(
+                            'Shop now and get value \nfor your quality products',
+                            style: TextStyle(
+                                color: Colors.black.withOpacity(0.5),
+                                fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 50),
+                          Container(
+                            height: 50,
+                            width: 300,
+                            decoration: BoxDecoration(
+                                color: Colors.blueAccent,
+                                borderRadius: BorderRadius.circular(30)),
+                            child: const Align(
+                                child: Text(
+                              'Get Started',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      )),
+                ),
+              ),
+            ]),
+          );
+        },
       ),
     );
   }
