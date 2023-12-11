e7334fbb23d7dffec516453534e30149cce2ab35
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-12-11 19:44:38 +0100

    Update splash_body.dart
    Refactoring code

--- lib/screen/splash/components/splash_body.dart
+++ lib/screen/splash/components/splash_body.dart
@@ -14,60 +14,19 @@ class _SplashBodyState extends State<SplashBody> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Stack(
-        children: [
-          Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                image: const AssetImage(
-                  'assets/images/splash/1.jpg',
-                ),
-                colorFilter: ColorFilter.mode(
-                    Colors.white.withOpacity(0.8), BlendMode.modulate),
-                fit: BoxFit.cover,
-              )),
-              child: Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.bottomRight,
-                        stops: const [
-                      0.2,
-                      0.9
-                    ],
-                        colors: [
-                      Colors.black.withOpacity(.6),
-                      Colors.black.withOpacity(.1),
-                    ])),
-              )),
-          const Positioned(
-            left: 80,
-            top: 400,
-            child: Column(
-              children: [
-                Text(
-                  'EMARK',
-                  style: Splash_textStyle,
-                ),
-                Divider(
-                    height: 5,
-                    color: Colors.white,
-                    thickness: 2,
-                    indent: 80,
-                    endIndent: 80),
-                Text(
-                  'Welcome to Emark!, \n Let\'s Shop',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22,
-                      fontWeight: FontWeight.w800),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+      child: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: const AssetImage(
+              'assets/images/splash/4.jpg',
             ),
-          ),
-          const SplashButton(),
-        ],
-      ),
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.modulate),
+            fit: BoxFit.cover,
+          )),
+        ),
+      ]),
     );
   }
 }
