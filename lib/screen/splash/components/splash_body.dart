1f8fa51ee1d42e16d8fb580b655a36d6910f9633
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-18 21:29:40 +0100

    update

--- lib/screen/splash/components/splash_body.dart
+++ lib/screen/splash/components/splash_body.dart
@@ -2,11 +2,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:animate_do/animate_do.dart';
-import 'package:emark/screen/home/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_carousel_slider/flutter_custom_carousel_slider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 
 class SplashBody extends StatefulWidget {
@@ -26,6 +22,87 @@ class _SplashBodyState extends State<SplashBody> {
 
 
   int currentIndex = 0; 
+
+@override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Stack(
+        children: [
+
+          PageView(
+            children:[ FadeInUp(
+              duration: Duration(milliseconds: 800),
+              child: Container(
+                width: double.infinity, 
+                height: double.infinity, 
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                  
+                  carousel[currentIndex][0]), 
+                fit: BoxFit.cover),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 400), 
+                  child: Padding(padding: const EdgeInsets.all(8.0), 
+                  child: Container(
+                    height: 300, width: 500,
+                    decoration: BoxDecoration(
+                      color: Colors.white, 
+                      borderRadius: BorderRadius.circular(30)
+                    ), 
+                     child: Column(
+                      children: [
+                        Padding(padding: const EdgeInsets.only(top: 10), 
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(padding: const EdgeInsets.only(right: 25.0), 
+                            child: GestureDetector(
+                              onTap: () {
+                                
+                              },
+                              child: const Text('Skip', style: TextStyle(fontSize: 16),
+                              ),
+                            ),)],
+                        ),), 
+                        const SizedBox(height: 8), 
+                        Expanded(
+                          child: FadeInUp(
+                            duration:  Duration(milliseconds: 1000),
+                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                              carousel[currentIndex][1], 
+                          style: const TextStyle(
+                            color: Colors.black,
+                             fontSize: 23,
+                              fontWeight: FontWeight.w700), 
+                          textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 20),
+                          Text(
+                            carousel[currentIndex][2], style: TextStyle(color: Colors.black.withOpacity(0.5), 
+                          fontSize: 16), 
+                          textAlign: TextAlign.center,), 
+                          const SizedBox(height: 20), 
+                           
+                        ],
+                       ),
+              ),
+            ),
+         ]
+         ) 
+         ))
+         )
+        
+        )) ] )]));
+
+          
+
+  }  
+  }
 
   // void _next() {
   //   setState(() {
@@ -71,34 +148,7 @@ class _SplashBodyState extends State<SplashBody> {
      
   //   ), 
   //]; 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        children: [
-
-          PageView(
-            children:[ Container(
-              width: double.infinity, 
-              height: double.infinity, 
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                
-                carousel[currentIndex][0]), 
-              fit: BoxFit.cover),
-              ),
-            ),
-         ] ),
-
-          
-          ]
-          )
-          );
-
-          
-  }
-
+  
   
 
        
