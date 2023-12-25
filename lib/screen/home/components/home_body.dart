395375f0834adf3ba2d81f9101587ca3222d3f6b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-25 12:42:24 +0100

    Adding Popular Products Category

--- lib/screen/home/components/home_body.dart
+++ lib/screen/home/components/home_body.dart
@@ -1,3 +1,5 @@
+import 'package:emark/models/product.dart';
+import 'package:emark/widgets/constants.dart';
 import 'package:flutter/material.dart';
 
 import 'home_widgets.dart';
@@ -27,7 +29,32 @@ class _BodyState extends State<Body> {
         const SizedBox(height: 20),
         SectionTitle(
           text: 'Popular Products',
-         press: (){})
+         press: (){}), 
+    SizedBox(
+      width: 140, 
+      child: Column(
+        children: [
+          AspectRatio(aspectRatio: 1.02, 
+          child: Container(
+            padding: EdgeInsets.all(20), 
+            decoration: BoxDecoration(
+              color: kSecondaryColor.withOpacity(0.1), 
+              borderRadius: BorderRadius.circular(15), 
+
+            ),
+            child: Image.asset(demoProducts[0].images[0]),
+
+          ),), 
+          Text(
+            demoProducts[0].title,
+             style: TextStyle(color: Colors.black),
+             maxLines: 2,
+          ), 
+          Text('${demoProducts[0].price}', style: TextStyle())
+
+        ],
+      )
+    )
      
       ],
     ),
