82ebd46592d2c074f6ddb46898319c2b8639edde
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-25 14:39:46 +0100

    Update nav_bar.dart

--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -1,4 +1,7 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+
+import 'constants.dart';
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
@@ -8,8 +11,44 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
+  int index = 0; 
+  final inactiveColor = Color(0xffffecdf); 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold( 
+bottomNavigationBar: BottomNavyBar(
+        backgroundColor: Colors.black,
+        containerHeight: 70,
+        selectedIndex: index,
+         onItemSelected: (index) => setState(()=> this.index = index), 
+        items: <BottomNavyBarItem>[
+
+          BottomNavyBarItem(
+            icon: const Icon(Icons.apps),
+           title: const Text('Home'), 
+           activeColor: kPrimaryColor,
+           inactiveColor: inactiveColor,
+           textAlign: TextAlign.center),   
+            BottomNavyBarItem(
+              icon: const Icon(Icons.shopping_cart),
+           title: const Text('Cart'), 
+           activeColor: kPrimaryColor,
+           inactiveColor: inactiveColor,
+           textAlign: TextAlign.center), 
+              BottomNavyBarItem(
+                icon: const Icon(Icons.favorite),
+           title: const Text('Saved'), 
+           activeColor: kPrimaryColor,
+           inactiveColor: inactiveColor,
+           textAlign: TextAlign.center),  
+             BottomNavyBarItem(
+              icon: const Icon(Icons.person),
+           title: const Text('Account'), 
+           activeColor: kPrimaryColor,
+           inactiveColor: inactiveColor,
+           textAlign: TextAlign.center), 
+        ],
+       ),
+    );
   }
 }
