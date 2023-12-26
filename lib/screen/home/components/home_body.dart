640317d8c3714a990aba8092e926bffbcdf86161
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-26 11:33:37 +0100

    Update home_body.dart

--- lib/screen/home/components/home_body.dart
+++ lib/screen/home/components/home_body.dart
@@ -1,5 +1,8 @@
+import 'package:emark/models/top_deals/appliances.dart';
+import 'package:emark/widgets/constants.dart';
 import 'package:flutter/material.dart';
 
+import '../../../models/top_deals/appliances.dart';
 import 'categories.dart';
 import 'collection_card.dart';
 import 'discount_banner.dart';
@@ -23,28 +26,28 @@ class _BodyState extends State<Body> {
         child: SingleChildScrollView(
             child: Column(
       children:  [
-        SizedBox(height: 20),
-        HomeHeader(),
-        SizedBox(height: 20),
-        DiscountBanner(),
-        SizedBox(height: 20),
-        Categories(),
-        SizedBox(height: 30),
-        SpeicialOffers(),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
+        const HomeHeader(),
+        const SizedBox(height: 20),
+        const DiscountBanner(),
+        const SizedBox(height: 20),
+        const Categories(),
+        const SizedBox(height: 30),
+        const SpeicialOffers(),
+        const SizedBox(height: 20),
        
-    PopularProducts(), 
-    SizedBox(height: 20), 
-    Padding(
+    const PopularProducts(), 
+    const SizedBox(height: 20), 
+    const Padding(
       padding: EdgeInsets.symmetric(horizontal: 20),
       child: Text('Our Collections', style: TextStyle(fontSize: 18),),
     ), 
     //const SizedBox(height: 2), 
-    CollectionCard(),
-    SizedBox(height: 20), 
+    const CollectionCard(),
+    const SizedBox(height: 20), 
     SectionTitle(text: 'Appliances Top Deals', press: (){}), 
-    SizedBox(height: 20), 
-    ApplianceTopCard(), 
+    const SizedBox(height: 20), 
+    const ApplianceTopCard(), 
     
      
       ],
@@ -57,10 +60,29 @@ class _BodyState extends State<Body> {
 }
 
 class ApplianceTopCard extends StatelessWidget {
-  const ApplianceTopCard({super.key});
+  const ApplianceTopCard({
+    super.key, 
+  this.width = 140,
+   this.aspectRatio = 1.02, 
+ required this.htopappliance, 
+   });
+
+final double width, aspectRatio; 
+final hTopAppliance htopappliance; 
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(left: 20), 
+      child: SizedBox(width: width, child: Column(
+        children: [
+          AspectRatio(aspectRatio: aspectRatio, child: Container(padding: const EdgeInsets.all(20), 
+          decoration: BoxDecoration(
+            color: kSecondaryColor.withOpacity(0.2), 
+            borderRadius: BorderRadius.circular(15),
+          ),),)
+        ],
+      ),),
+    );
   }
 }
