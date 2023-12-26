e9225c90dc0ce0754419d37c17d1e7094ff4c4c1
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-26 10:39:45 +0100

    Update appliances.dart
    Writing Class

--- lib/models/top_deals/appliances.dart
+++ lib/models/top_deals/appliances.dart
@@ -1,10 +1,22 @@
+
 class TopAppliance {
   final String title, description; 
-  final List<String> images; 
+  final List<String> image; 
   final double price;
 
-  TopAppliance(this.price, {required this.title, required this.description, required this.images}); 
-
-
+  TopAppliance({required this.price, required this.title, required this.description, required this.image}); 
 
 }
+
+
+
+    List<TopAppliance> topAppliances = [
+      TopAppliance(
+        // id: 1, 
+        image: ['assets/images/appliance/air_condition.png'], 
+        price: 30.44,
+        title: "Aeon 1Hp Split Air Condition", 
+        description: 'This is the best product ever made', 
+      )
+    ];
+       
