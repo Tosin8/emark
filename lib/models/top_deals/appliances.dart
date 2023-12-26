6e9135627d3f9d4bea3f8d7646b681b0c5e83921
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-26 10:25:37 +0100

    Update appliances.dart

---
+++ lib/models/top_deals/appliances.dart
@@ -0,0 +1,10 @@
+class TopAppliance {
+  final String title, description; 
+  final List<String> images; 
+  final double price;
+
+  TopAppliance(this.price, {required this.title, required this.description, required this.images}); 
+
+
+
+}
