9f740b23f30a559888183a4b94b7f9418a7a2874
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-28 22:32:56 +0100

    Update details_screen.dart
    Done with App bar

--- lib/screen/detail_product/details_screen.dart
+++ lib/screen/detail_product/details_screen.dart
@@ -14,30 +14,37 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20), 
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              onPressed:()=> Navigator.pop(context), 
-              icon: const Icon(Icons.arrow_back_ios), 
-              ),
-              Container(padding: EdgeInsets.symmetric(horizontal: 14, vertical: 5), 
-              decoration: BoxDecoration(color: Colors.white),
+        leading: 
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: IconButton(
+                onPressed:()=> Navigator.pop(context), 
+                icon: const Icon(Icons.arrow_back_ios), 
+                ),
+            ),     
+        actions: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 5), 
+              decoration:  BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.white, 
+                boxShadow: [
+                  BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 1, blurRadius: 2)
+                ]),
 
                 child: Row(
                   children: [
-                    Text('4.5', style: TextStyle(fontWeight: FontWeight.w600),), 
-                    SizedBox(width: 5), 
+                    const Text('4.5', style: TextStyle(fontWeight: FontWeight.w600),), 
+                    const SizedBox(width: 5), 
                     SvgPicture.asset('assets/icons/star_icon.svg')
                   ],
                 )
-              )
+              ),
+              SizedBox(width: 10,)
           ],
         ),
-      ),
-     ) );
+      );
+     
   }
 }
 
