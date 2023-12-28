a94270945b63044c57bf59175c3e94fa16dea1e0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-28 23:04:00 +0100

    Update details_screen.dart

--- lib/screen/detail_product/details_screen.dart
+++ lib/screen/detail_product/details_screen.dart
@@ -11,60 +11,40 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: CustomAppBar(
-        preferredSize: Size.fromHeight(AppBar().preferredSize.height), 
-        
-        child: 4.6,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: 
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: IconButton(
+                onPressed:()=> Navigator.pop(context), 
+                icon: const Icon(Icons.arrow_back_ios), 
+                ),
+            ),     
+        actions: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 5), 
+              decoration:  BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.white, 
+                boxShadow: [
+                  BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 1, blurRadius: 2)
+                ]),
+
+                child: Row(
+                  children: [
+                    const Text('4.5', style: TextStyle(fontWeight: FontWeight.w600),), 
+                    const SizedBox(width: 5), 
+                    SvgPicture.asset('assets/icons/star_icon.svg')
+                  ],
+                )
+              ),
+              SizedBox(width: 20,)
+          ],
         ),
       );
      
-  }
-}
-
-class CustomAppBar extends PreferredSize {
-  final double rating; 
-  const CustomAppBar(this.rating, {
-    super.key, required super.preferredSize, required super.child,
-  });
-
-  @override
-  Size get preferredSize => Size.fromHeight(AppBar().preferredSize.height);
-  Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      leading: 
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: IconButton(
-              onPressed:()=> Navigator.pop(context), 
-              icon: const Icon(Icons.arrow_back_ios), 
-              ),
-          ),     
-      actions: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 5), 
-            decoration:  BoxDecoration(
-              borderRadius: BorderRadius.circular(6),
-              color: Colors.white, 
-              boxShadow: [
-                BoxShadow(color: Colors.black.withOpacity(0.3),spreadRadius: 1, blurRadius: 2)
-              ]),
-    
-              child: Row(
-                children: [
-                  Text(
-                    
-                    rating.toString(), 
-                    style: const TextStyle(fontWeight: FontWeight.w600),), 
-                  const SizedBox(width: 5), 
-                  SvgPicture.asset('assets/icons/star_icon.svg')
-                ],
-              )
-            ),
-            const SizedBox(width:30,)
-        ],
-      );
   }
 }
 
