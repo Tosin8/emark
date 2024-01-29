9e178bc6903f32ebf40069f85357487bb709a7d6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-29 22:06:34 +0100

    Update body.dart

--- lib/screen/account/components/body.dart
+++ lib/screen/account/components/body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'profile_menu.dart';
-import 'profile_pic.dart';
+import 'profile_heading.dart';
 
 class AccountBody extends StatelessWidget {
   const AccountBody({super.key});
@@ -11,35 +11,28 @@ class AccountBody extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          Container(
-            
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundImage: const AssetImage('assets/images/sub-image/user_profile.jpg', ),
-                radius: 40,
-                backgroundColor: Theme.of(context).primaryColorDark,
-                
-              ),
-              title: Text('Lizzy Jones', style: TextStyle(color: Colors.black, fontSize: 18),),
-              subtitle: Text('lizzyjones@gmail.com'),
-              trailing: IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
-            ),
-          ),
-          SizedBox(height: 10), 
+          const profile_heading(),
+          const SizedBox(height: 10), 
           Container( 
            
             width: double.infinity, 
             height: 800,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(20) , 
               topRight: Radius.circular(20)),  color: Colors.black,
           ),
-            child: Text('Hello'),
+            child: const Column(
+              children: [
+                SizedBox(height: 12), 
+                
+                  Text('Account Settings', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),)
+              ],
+            ),
           )
         ],
-      )
-    );
+      
+    )); 
   }
 }
 
